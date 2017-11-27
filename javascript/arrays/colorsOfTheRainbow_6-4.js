8b9a428328af4b317214f84ba1aeaf8db48c0f13
cvicky <c.vicky1213@gmail.com>
2017-11-27 00:01:16 -0500

    Create colorsOfTheRainbow_6-4.js

---
+++ javascript/arrays/colorsOfTheRainbow_6-4.js
@@ -0,0 +1,15 @@
+/*
+ * Programming Quiz: Colors of the Rainbow (6-4)
+
+Remove "Blackberry"
+Add "Yellow" and "Green"
+Add "Purple"
+
+ */
+
+var rainbow = ["Red", "Orange", "Blackberry", "Blue"];
+
+// your code goes here
+rainbow.splice(2, 1, "Yellow", "Green");
+rainbow.push("Purple");
+console.log(rainbow);
